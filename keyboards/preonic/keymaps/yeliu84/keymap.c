d9f1e8a15bd6db430041add877ffad87d457b95e
Ye Liu <yeliu84@gmail.com>
2020-09-14 23:41:25 -0400

    update preonic keymap

--- keyboards/preonic/keymaps/yeliu84/keymap.c
+++ keyboards/preonic/keymaps/yeliu84/keymap.c
@@ -115,11 +115,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_preonic_grid( \
-  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC, \
-  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,  \
-  KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
-  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,S(KC_NUHS),S(KC_NUBS),KC_HOME, KC_END, _______, \
-  _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
+  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,    KC_PERC,      KC_CIRC, KC_AMPR,       KC_ASTR,       KC_UNDS, KC_PLUS, _______, \
+  _______, _______, _______, _______, LCA(KC_R), LALT(KC_F12), _______, _______,       _______,       KC_MINS, KC_EQL,  _______, \
+  _______, _______, _______, _______, _______,   _______,      _______, SGUI(KC_LBRC), SGUI(KC_RBRC), KC_LCBR, KC_RCBR, _______, \
+  _______, _______, _______, _______, _______,   _______,      _______, _______,       _______,       KC_LBRC, KC_RBRC, _______, \
+  _______, _______, _______, _______, _______,   _______,      _______, _______,       KC_HOME,       KC_PGUP, KC_PGDN, KC_END   \
 ),
 
 /* Raise
@@ -136,11 +136,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_preonic_grid( \
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,  \
-  KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
-  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______, \
-  _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY  \
+  KC_TILD, KC_F1,      KC_F2,      KC_F3,   KC_F4,        KC_F5,        KC_F6,   KC_F7,   KC_F8,               KC_F9,                KC_F10,           KC_F11,  \
+  KC_TAB,  LCA(KC_Q),  _______,    _______, LALT(KC_F11), SGUI(KC_F12), _______, _______, _______,             _______,              SGUI(LALT(KC_P)), KC_F12,  \
+  KC_CAPS, SGUI(KC_A), SGUI(KC_F), _______, LALT(KC_F1),  _______,      _______, _______, LALT(LGUI(KC_K)),    _______,              _______,          _______, \
+  KC_LSFT, _______,    _______,    _______, _______,      _______,      _______, _______, LALT(LGUI(KC_LEFT)), LALT(LGUI(KC_RIGHT)), LALT(LGUI(KC_F)), _______, \
+  _______, _______,    _______,    _______, _______,      _______,      _______, _______, _______,             _______,              _______,          _______  \
 ),
 
 /* Adjust (Lower + Raise)
